92328c196ab8c07d4376d757f480af29136a6c13
Михаил Калмыков <68296867+Mike-Vazovskiy@users.noreply.github.com>
2023-08-04 19:16:55 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,15 +5,16 @@ let downArrow = document.getElementById("downArrow")
 let x = 0;
 
 upArrow.addEventListener("click", function() {
-    if(x > "-900"){
-        x = x - 300;
+    if(x < 0){
+        x = x + 300;
         slide.style.top = x + "px"
     }
 })
 
 downArrow.addEventListener("click", function() {
-    if(x < 0){
-        x = x + 300;
+    
+    if(x > "-900"){
+        x = x - 300;
         slide.style.top = x + "px"
     }
 })
